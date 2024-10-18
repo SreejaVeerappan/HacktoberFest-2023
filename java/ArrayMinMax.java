b82f8359610e87af9cdcedc3f8d4ccefd745270d
SreejaVeerappan <144646423+SreejaVeerappan@users.noreply.github.com>
2024-10-19 03:27:56 +0530

    Update ArrayMax.java

--- java/ArrayMinMax.java
+++ java/ArrayMinMax.java
@@ -1,31 +1,36 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class ArrayMinMax {
-    static void getMinMax(long a[], long n)  
-    {
-        long maxNo=Integer.MIN_VALUE;
-        long minNo=Integer.MAX_VALUE;
-        for(int i=0;i<n;i++){
-            if(a[i]>maxNo){
-                maxNo=a[i];
+
+    public static void getMinMax(long[] a) {
+        long maxNo = Long.MIN_VALUE; // Use Long.MIN_VALUE for long data type
+        long minNo = Long.MAX_VALUE;
+
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] > maxNo) {
+                maxNo = a[i];
             }
-            if(a[i]<minNo){
-                minNo=a[i];
+            if (a[i] < minNo) {
+                minNo = a[i];
             }
         }
-        System.out.println("min = "+minNo);
-        System.out.println("max = "+maxNo);
+
+        System.out.println("min = " + minNo);
+        System.out.println("max = " + maxNo);
     }
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        long a[]=new long[n];
-        for(int i=0;i<n;i++){
-            a[i]=sc.nextInt();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        long[] a = new long[n];
+
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextLong(); // Use nextLong() for long data type
         }
-        getMinMax(a,n);
-        Arrays.sort(a);
+
+        getMinMax(a); // No need to pass array length as parameter
+
+        // Print the fourth element (assuming indexing starts from 0)
         System.out.println(a[3]);
     }
 }
