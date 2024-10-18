750f4c1088ced46bb1d1dc9fcd45f6d342d1ee07
SreejaVeerappan <144646423+SreejaVeerappan@users.noreply.github.com>
2024-10-18 17:26:15 +0530

    Insertion Sort Algorithm in C.c

--- C/Insertion Sort Algorithm.c
+++ C/Insertion Sort Algorithm.c
@@ -1,42 +1,42 @@
-// C program for insertion sort
-#include <math.h>
 #include <stdio.h>
 
-/* Function to sort an array using insertion sort*/
-void insertionSort(int arr[], int n)
-{
-	int i, key, j;
-	for (i = 1; i < n; i++) {
-		key = arr[i];
-		j = i - 1;
-		/* Move elements of arr[0..i-1], that are
-		greater than key, to one position ahead
-		of their current position */
-		while (j >= 0 && arr[j] > key) {
-			arr[j + 1] = arr[j];
-			j = j - 1;
-		}
-		arr[j + 1] = key;
-	}
+// Function to perform insertion sort
+void insertionSort(int array[], int size) {
+    for (int i = 1; i < size; i++) {
+        int key = array[i];
+        int j = i - 1;
+
+        // Move elements of array[0..i-1], that are greater than key,
+        // to one position ahead of their current position
+        while (j >= 0 && array[j] > key) {
+            array[j + 1] = array[j];
+            j = j - 1;
+        }
+        array[j + 1] = key;  // Place the key at its correct position
+    }
 }
 
-// A utility function to print an array of size n
-void printArray(int arr[], int n)
-{
-	int i;
-	for (i = 0; i < n; i++)
-		printf("%d ", arr[i]);
-	printf("\n");
+// Function to print the array
+void printArray(int array[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
 }
 
-/* Driver program to test insertion sort */
-int main()
-{
-	int arr[] = { 12, 11, 13, 5, 6 };
-	int n = sizeof(arr) / sizeof(arr[0]);
+int main() {
+    int array[] = {12, 11, 13, 5, 6};
+    int size = sizeof(array) / sizeof(array[0]);
 
-	insertionSort(arr, n);
-	printArray(arr, n);
+    printf("Unsorted array: ");
+    printArray(array, size);
 
-	return 0;
+    // Perform insertion sort
+    insertionSort(array, size);
+
+    printf("Sorted array: ");
+    printArray(array, size);
+
+    return 0;
 }
+
