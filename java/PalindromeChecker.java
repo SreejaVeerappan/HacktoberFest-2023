b1ac11882b177c39d5ad111d77c4a0b83c91e232
SreejaVeerappan <144646423+SreejaVeerappan@users.noreply.github.com>
2024-10-18 18:00:21 +0530

    PalindromeChecker.java

--- java/PalindromeChecker.java
+++ java/PalindromeChecker.java
@@ -1,21 +1,31 @@
 import java.util.Scanner;
 
-class PalindromeChecker {
-    public static void main(String[] args){
-        
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a string:");
-        String s = sc.nextLine();
-        
-        for(int i=0; i<s.length()/2; i++)
-        {
-            if(s.charAt(i)!=s.charAt(s.length()-i-1))
-            {
-                System.out.println('"'+s+'"'+" is not a palindrome string.");
-                return;
+public class PalindromeChecker {
+    public static boolean isPalindrome(String str) {
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
             }
+            left++;
+            right--;
         }
-        
-        System.out.println('"'+s+'"'+" is a palindrome string.");
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a word or phrase: ");
+        String input = scanner.nextLine();
+
+        if (isPalindrome(input)) {
+            System.out.println("It's a palindrome!");
+        } else {
+            System.out.println("It's not a palindrome.");
+        }
     }
 }
